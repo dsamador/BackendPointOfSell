9fb643374668b4a910bcf1e1d1114c776a3d107f
David Amador <54488581+dsamador@users.noreply.github.com>
2022-12-22 22:29:47 -0500

    Validating

---
+++ Application/Features/Categories/Commands/CreateCategoryCommand/CreateCategoryCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Application.Features.Categories.Commands.CreateCategoryCommand
+{
+    public class CreateCategoryCommandValidator: AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator() 
+        {
+            RuleFor(c => c.Name)
+                .MaximumLength(40)
+                .WithMessage("La {PropertyName} no puede exeder de 40 caracteres")
+                .NotEmpty()
+                .WithMessage("La {PropertyName} no puede quedar vacía");
+        }
+    }
+}
